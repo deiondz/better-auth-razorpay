c429e529078e1f60ca49095bf77769b8168268a9
Deion Dsouza <deiondsouza12@gmail.com>
2026-02-02 21:54:45 +0530

    Increment package version to 2.0.31 in package.json and package-lock.json; update cancelSubscription function to invert cancelAtPeriodEnd logic.

--- api/cancel-subscription.ts
+++ api/cancel-subscription.ts
@@ -64,7 +64,7 @@ export const cancelSubscription = (razorpay: Razorpay) =>
           where: [{ field: 'id', value: body.subscriptionId }],
           update: {
             data: {
-              cancelAtPeriodEnd: body.immediately,
+              cancelAtPeriodEnd: !body.immediately,
               updatedAt: new Date(),
             },
           },
